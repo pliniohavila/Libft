3f7a3a152482fffade4b4072ee30edf3fb3b7a4c
pliniocode <pliniohrcode@gmail.com>
2023-12-06 17:08:34 -0300

    Make ft_memcmp

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -1,23 +1,4 @@
-#include <stdio.h>
-#include <string.h>
 #include "libft.h"
-
-#define SIZE 30
-
-
-int     main(void)
-{
-    char    buf1[SIZE];
-    char    buf2[SIZE];
-
-    memset(buf1, 0, SIZE);
-    memset(buf2, 0, SIZE);
-    strcpy(buf1, "Chiquita and Mel!");
-    strcpy(buf2, "Chiquita end Mel!");
-    printf("memcmp: %d\n", memcmp(buf1, buf2, strlen(buf1)));
-    printf("ft_memcmp: %d\n", ft_memcmp(buf1, buf2, strlen(buf1)));
-    return (0);
-}
 
 int     ft_memcmp(const void *s1, const void *s2, size_t n)
 {
