a28e81faf50e9930e3b6a7fe5bf874fee9722687
pliniohavila <senhor@DESKTOP-SDM2J3U>
2023-12-15 12:43:00 -0300

    Start ft_itoa

--- libft/main.c
+++ libft/main.c
@@ -7,75 +7,64 @@
 
 int     main(void)
 {
-    char    *buf1 = "In this example, two arrays of 14 characters each are created. The contents of the string literals blah and hmm are copied into these arrays ";
-    char    **buf2;
+    // char    *buf1;
+    // char    *buf2;
 
     // memset(buf1, 0, SIZE);
     // memset(buf2, 0, SIZE);
     // strcpy(buf2, " vtr");
-    buf2 = ft_split(buf1, 32);
-    for (int i = 0; buf2[i] != NULL; i++)
-        printf("%s - ", buf2[i]);
-    // printf("[INFO] %s\n", buf1);
-    // printf("[INFO] %s\n", ft_strtrim(buf1, buf2));
+    // buf2 = ft_split(buf1, 32);
+    // for (int i = 0; buf2[i] != NULL; i++)
+    //     printf("%s - ", buf2[i]);
+    // printf("[INFO] %s\n", ft_itoa(1212));
+    // printf("[INFO] %s\n", ft_itoa(42));
+    // printf("[INFO] %s\n", ft_itoa(4));
+    // printf("[INFO] %s\n", ft_itoa(0));
+    printf("[INFO] %s\n", ft_itoa(-42));
     printf("\n");
     return (0);
 }
 
-static int     get_qtd_split(char const *s, char c)
+static int      c_digits(int n)
 {
-    int         i;
-    int         qtd_split;
-
-    i = 0;
-    qtd_split = 0;
-    while(s[i] != '\0') 
+    int     count_dgts;
+    
+    count_dgts = 0;
+    while (n > 0)
     {
-        if (s[i] == c)
-            qtd_split++;
-        i++;
+        count_dgts++;
+        n = (n - (n % 10)) / 10;
     }
-    return (i);
+    return (count_dgts);
 }
 
-static  char    *str_splitted(const char *s, int start, int end)
+char    *ft_itoa(int n)
 {
-    char        *str;
+    int     count_dgts;
+    int     negative;
+    char    *itoa;
 
-    str = (char*)malloc(sizeof(char) * ((end - start) + 1));
-    ft_strlcpy(str, (s + start), ((end - start) + 1));
-    return (str);
-}
-
-char    **ft_split(char const *s, char c)
-{
-    int         i;
-    int         k;
-    int         qtd_split;
-    int         start;
-    char        **divided;
-
-    qtd_split = get_qtd_split(s, c);
-    divided = (char**)malloc(sizeof(char*) * (qtd_split + 2)); // sum 2 to get last word and set null 
-    if (!divided)
+    negative = 0;
+    count_dgts = 0;
+    if (n < 0)
+    {
+        negative = 1;
+        count_dgts++;
+        n = n * (-1);
+    }
+    count_dgts += (c_digits(n) + 1);   
+    // printf("count_dgts: %d\n", count_dgts); 
+    itoa = (char*)malloc(sizeof(char) * count_dgts);
+    if (!itoa)
         return (NULL);
-    i = 0;
-    k = 0;
-    start = 0;
-    while (s[i] != '\0')
+    if (negative)
+        itoa[0] = '-';
+    itoa[count_dgts--] = '\0';
+    while (n > 0)
     {
-        if (s[i] == c) 
-        {
-            if ((divided[k] = str_splitted(s, start, i)) == NULL) 
-                return (NULL);
-            start = i + 1;
-            k++;
-        }
-        i++;
+        itoa[count_dgts--] = (char)((n % 10) + 48);
+        printf("itoa: %s\n", itoa); 
+        n = (n - (n % 10)) / 10;
     }
-    if (ft_strlen(s + start) > 0)
-        if ((divided[k++] = str_splitted(s, start, i)) == NULL) 
-                return (NULL);
-    divided[k] = NULL;
-    return (divided);
+    return (itoa);
 }
