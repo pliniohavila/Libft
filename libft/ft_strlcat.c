cfdd1eaaa10ba5acb8b52841774bb853419e3030
pliniohavila <pliniohrcode@gmail.com>
2023-12-26 07:42:52 -0300

    Update ft_strlcat code

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -3,14 +3,21 @@
 size_t  ft_strlcat(char *dst, const char *src, size_t n)
 {
     size_t      i;
-
+    size_t      k;
+    size_t      len_src;
+    
+    len_src = ft_strlen(src);
     if ((!dst) && (!src))
         return (0);
     if (n == 0)
         return (ft_strlen(src));
     i = ft_strlen(dst);
-    while ((*src != '\0') && (i < (n - 1)))
+    k = 0;
+    while ((*src != '\0') && (k < n))
+    {
         dst[i++] = *src++;
+        k++;
+    }
     dst[i] = '\0';
-    return (n + ft_strlen(src));
+    return (n + len_src);
 }
